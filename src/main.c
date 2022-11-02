18f1e50902bfa97de4f00ff99ba0a7e6a4f3bef8
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-11-02 18:35:51 +0700

    bagusin directory

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include "./adt/kata/kata.h"
 #include "./adt/arraydin/arraydin.h"
 
-void commandInput(Word command) {
-    
+void commandInput(Word command)
+{
 }
 
-int main() {
+int main()
+{
     boolean quit = false;
     ArrayDin arrGame = MakeArrayDin();
     ArrayDin arrHistory = MakeArrayDin();
@@ -16,7 +17,8 @@ int main() {
     printf("\n======== SELAMAT DATANG DI BNMO ========\n");
     printf("\n=========== TUBES ALSTRUKDAT ===========\n");
 
-    while (!quit) {  
+    while (!quit)
+    {
         printf("\nENTER COMMAND: ");
         Word command;
         startInputWord();
@@ -24,60 +26,85 @@ int main() {
 
         Word cek;
         akuisisiCommandWord(&cek, command);
-        if (stringEQWord(cek, "LOAD") || stringEQWord(cek, "SAVE")) {
-            char* file;
+        if (stringEQWord(cek, "LOAD") || stringEQWord(cek, "SAVE"))
+        {
+            char *file;
             file = akuisisiFile(command);
-            if (stringEQWord(cek, "LOAD")) {
+            if (stringEQWord(cek, "LOAD"))
+            {
                 load(concat("../data/", file), &arrGame, &arrHistory);
                 printf("Save file berhasil dibaca. BNMO berhasil dijalankan.\n\n");
-            } else if (stringEQWord(cek, "SAVE")) {
+            }
+            else if (stringEQWord(cek, "SAVE"))
+            {
                 /* BELUM ADA */
                 printf("Save file berhasil disimpan..\n\n");
             }
-        } else {
-            if (stringEQWord(command, "START")) {
+        }
+        else
+        {
+            if (stringEQWord(command, "START"))
+            {
                 load("../data/default.txt", &arrGame, &arrHistory);
                 printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.\n\n");
-            } else if (stringEQWord(command, "QUIT")) {
+            }
+            else if (stringEQWord(command, "QUIT"))
+            {
                 printf("\nAnda keluar dari game BNMO.\nBye bye ...\n\n");
                 quit = true;
-            } else if (stringEQWord(command, "CREATE GAME")) {
-                printf("Masukkan nama game yang akan ditambahkan: ");;;
+            }
+            else if (stringEQWord(command, "CREATE GAME"))
+            {
+                printf("Masukkan nama game yang akan ditambahkan: ");
+                ;
+                ;
                 Word newGame;
                 startInputWord();
                 newGame = currentWord;
                 InsertLast(&arrGame, newGame);
                 printf("Game berhasil ditambahkan\n");
-            } else if (stringEQWord(command, "QUEUE GAME")) {
+            }
+            else if (stringEQWord(command, "QUEUE GAME"))
+            {
                 /* BELUM ADA */
-            } else if (stringEQWord(command, "PLAY GAME")) {
+            }
+            else if (stringEQWord(command, "PLAY GAME"))
+            {
                 /* BELUM ADA */
-            } else if (stringEQWord(command, "LIST GAME")) {
+            }
+            else if (stringEQWord(command, "LIST GAME"))
+            {
                 printf("Berikut adalah daftar game yang tersedia\n\n");
                 PrintArrayDin(arrGame);
                 printf("\n");
-            } else if (stringEQWord(command, "DELETE GAME")) {
+            }
+            else if (stringEQWord(command, "DELETE GAME"))
+            {
                 printf("Berikut adalah daftar game yang tersedia\n\n");
                 PrintArrayDin(arrGame);
                 printf("\nMasukan game yang akan dihapus: ");
                 int numGame;
                 scanf("%d", &numGame);
-                if (numGame > 5 && arrGame.Neff>5) {
-                    DeleteAt(&arrGame, numGame-1);
+                if (numGame > 5 && arrGame.Neff > 5)
+                {
+                    DeleteAt(&arrGame, numGame - 1);
                     printf("\nGame berhasil dihapus\n");
-                } else {
+                }
+                else
+                {
                     printf("\nGame gagal dihapus\n");
                 }
-            } else if (stringEQWord(command, "HELP")) {
+            }
+            else if (stringEQWord(command, "HELP"))
+            {
                 /* BELUM ADA */
-            } else {
+            }
+            else
+            {
                 printf("Command tidak dikenali, silahkan masukkan command yang valid.\n\n");
-            }    
+            }
         }
-        
     }
-
 
     return 0;
 }
-
