f8eee4619ef3762f4165884ab9ff3190ab12b2dd
RClyf <18221084@std.stei.itb.ac.id>
2022-11-21 22:52:08 +0700

    hangman setengah jadi

--- src/games/hangman/hangman.c
+++ src/games/hangman/hangman.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include "../../adt/mesin/mesinkarakter.c"
-#include "../../adt/mesin/mesinkata.c"
-#include "../../adt/arraydin/arraydin.c"
-#include "../../adt/word/word.c"
-#include "../../adt/string/string.c"
+#include "hangman.h"
 
 char* kata_random()
 {
@@ -30,17 +27,146 @@ char* kata_random()
     srand(time(NULL));
     x = rand() % total;
 
-    printf("%d\n", x);
-    int j;
+    int j,num;
+    num = 0;
     char* kata = malloc (sizeof (char) * BUFFER(arrKata)[x].Length+1);
     for (j = 0; j < BUFFER(arrKata)[x].Length; j++)
     {
         kata[j] = BUFFER(arrKata)[x].TabWord[j];
+        num++;
     }
     kata[BUFFER(arrKata)[x].Length] = '\0';
     
     return kata;
 }
+
+int check(char* kata, char huruf)
+{
+    int i,length;
+    length = stringLength(kata);
+
+    for (i = 0; i < length; i++)
+    {
+        if (kata[i] == huruf)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int isWin(char* kata)
+{
+    int i,length;
+    length = stringLength(kata);
+
+    for (i = 0; i < length; i++)
+    {
+        if (kata[i] == '_')
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int play()
+{
+    char tebakan[11];
+    char* kata;
+    int i, j;
+    int panjang_kata, found, win;
+
+    kata = kata_random();
+    panjang_kata = stringLength(kata);
+    char word[panjang_kata+1];
+    printf("%d %s\n", panjang_kata, kata);
+
+    for (i = 0; i < panjang_kata; i++)
+    {
+        word[i] = '_';
+    }
+    word[panjang_kata] = '\0';
+    printf("Tebakan sebelumnya: -\n");
+    printf("Kata: %s\n", word);
+    printf("Kesempatan: %d\n", 10);
+
+    Word temp;
+    char* input;
+    printf("Masukkan tebakan (DALAM HURUF KAPITAL): ");
+    startInputWord();
+    akuisisiCommandWord(&temp, currentWord, 1);
+    input = wordToString(temp);
+
+    tebakan[0] = *input;
+    tebakan[1] = '\0';
+
+    found = check(kata, *input);
+    if (found)
+    {
+        for (i = 0; i < panjang_kata; i++)
+        {
+            if (kata[i] == *input)
+            {
+                word[i] = *input;
+            }
+        }
+    }
+    printf("\n");
+
+    for (j = 1; j < 10; j++)
+    {
+        printf("Tebakan sebelumnya: %s\n", tebakan);
+
+        printf("Kata: %s\n", word);
+
+        printf("Kesempatan: %d\n", 10-j);
+
+        printf("Masukkan tebakan (DALAM HURUF KAPITAL): ");
+        
+        startInputWord();
+        akuisisiCommandWord(&temp, currentWord, 1);
+        input = wordToString(temp);
+
+        tebakan[j] = *input;
+        tebakan[j+1] = '\0';
+
+        found = check(kata, *input);
+
+        if (found)
+        {
+            for (i = 0; i < panjang_kata; i++)
+            {
+                if (kata[i] == *input)
+                {
+                    word[i] = *input;
+                }
+            }
+        }
+
+        win = isWin(word);
+        printf("\n");
+        if (win == 1)
+        {
+            break;
+        }
+        
+        
+    }
+
+    if (win == 1)
+    {
+        printf("Berhasil menebak kata %s! Kamu mendapatkan poin!\n\n", word);
+    }
+    else
+    {
+        printf("Jangan menyerah, silahkan coba bermain lagi!\n\n");
+    }
+
+    return 0;
+
+}
+
 
 
 
---
+++ src/games/hangman/hangman.h
@@ -0,0 +1,18 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+#include "../../boolean.h"
+#include "../../adt/mesin/mesinkarakter.h"
+#include "../../adt/mesin/mesinkata.h"
+#include "../../adt/arraydin/arraydin.h"
+#include "../../adt/word/word.h"
+#include "../../adt/string/string.h"
+
+char* kata_random();
+
+int check(char* kata, char huruf);
+
+int isWin(char* kata);
+
+int play();
+
+#endif
