e326887cc1c0a9cbe8698fc1d8cdac2461c6bb52
RClyf <18221084@std.stei.itb.ac.id>
2022-11-24 21:52:51 +0700

    Finishing Hangman

--- src/games/hangman/hangman.c
+++ src/games/hangman/hangman.c
@@ -194,9 +194,106 @@ int play()
 
     printf("Kesempatan kamu untuk menebak sudah habis. Selamat kamu berhasil mengumpulkan %d poin!\n\n", score);
 
-    return 0;
+    return score;
 
 }
+
+void saveDict(ArrayDin arr, FILE *pita)
+{
+    int i, j;
+    Word count;
+    count = intToWord(arr.Neff);
+    for (i = 0; i < count.Length; i++)
+    {
+        fprintf(pita, "%c", count.TabWord[i]);
+    }
+    if (arr.Neff != 0)
+    {
+        fprintf(pita, "\n");
+    }
+
+    for (i = 0; i < arr.Neff; i++)
+    {
+        for (j = 0; j < arr.A[i].Length; j++)
+        {
+            fprintf(pita, "%c", arr.A[i].TabWord[j]);
+        }
+        if (i < arr.Neff - 1)
+        {
+            fprintf(pita, "\n");
+        }
+    }
+}
+
+void tambahKata()
+{
+    Word temp, eop, total;
+    char* input;
+    int i, lower;
+    do
+    {
+        printf("\nMasukkan kata baru (DALAM HURUF BESAR): ");
+        startInputWord();
+        akuisisiCommandWord(&temp, currentWord, 1);
+        input = wordToString(temp);\
+        lower = 0;
+        for (i = 0; i < stringLength(input); i++)
+        {
+            lower = isLower(input[i]);
+            if (lower == 1)
+            {
+                break;
+            }
+        }
+    } while (lower == 1);
+    
+    int count; 
+    ArrayDin arrKata;
+
+    arrKata = MakeArrayDin();
+    STARTWORD("kata.txt");
+
+    if (!EOP)
+    {
+        count = wordToInt(currentWord);
+        int i;
+        for (i = 0; i < count; i++)
+        {
+            ADVWORD();
+            InsertAt(&arrKata, currentWord, i);
+        }
+        ADVWORD();
+    }
+
+    InsertAt(&arrKata, temp, count);
+    
+    FILE *pita;
+    pita = fopen("kata.txt", "w");
+
+    saveDict(arrKata,pita);
+
+    fclose(pita);
+    printf("\nKata berhasil ditambahkan.\n");
+
+}
+
+void hangman(int *score, char* input)
+{
+    if(input == 'PLAY')
+    {
+        play();
+    }
+    else if(input == 'TAMBAH KATA')
+    {
+        tambahKata();
+    }
+    else
+    {
+        printf("Input anda salah. Silahkan masukan command yang benar.\n");
+    }
+}
+
+
 
 
 
